3d3bdb6f17c77cbd562e09f7129e6b98156754e6
Zachary Warnes <zacharywarnes@gmail.com>
2022-10-12 17:33:34 -0700

    adding post route and working on put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,13 +37,32 @@ router.get('/:id', async (req, res) => {
   
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+// router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-});
+//   try {
+//     const categoryData = await Category.update(
+//       {
+//         category_name: req.body.category_name,
+//     },
+//     {
+//       where: {
+//         id: req.params.id,
+//       },
+//     }
+//     )
+//     .then(())
+//   }
+// });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
